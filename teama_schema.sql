a8e79345881cbee4fa3106be07cba4fdfc5ed3b7
Will J Dunn <WillJDunn@users.noreply.github.com>
2019-03-30 16:40:29 -0400

    Update teama_schema.sql
    - Now supports SHA2-256 encrypted passwords!
      - Procedures add_user and edit_user will encrypt the input variable in_password and store the encrypted password in the users table. 
      - New procedures check_user_password_by_user_id, check_user_password_by_user_name, check_user_by_password_email can be used to check if a provided password matches the one in users.password
    - Column comments.comment has been changed to datatype TEXT (increase max length, can be changed via "ALTER TABLE comments MODIFY COLUMN comment TEXT;")
    - Additional stored procedures have been added for editing table data

--- teama_schema.sql
+++ teama_schema.sql
@@ -1,4 +1,4 @@
--- Updated Fri 29 Mar 2019 02:09:30 PM PDT
+-- Updated Sat 30 Mar 2019 01:32:15 PM PDT
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -22,10 +22,10 @@ DROP TABLE IF EXISTS `teama`.`users` ;
 CREATE TABLE IF NOT EXISTS `teama`.`users` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(45) NOT NULL,
-  `password` VARBINARY(256) NOT NULL,
+  `password` VARBINARY(256) NULL,
   `email` VARCHAR(45) NOT NULL,
   `email_verified` TINYINT NULL DEFAULT 0,
-  `registered_at` DATETIME NULL,
+  `registered_at` DATETIME NULL DEFAULT NOW(),
   `deactivated` TINYINT NULL DEFAULT 0,
   PRIMARY KEY (`user_id`),
   INDEX `idx_user_name` (`user_name` ASC),
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `teama`.`project_users` (
   `user_id` INT NOT NULL,
   `is_admin` TINYINT NULL DEFAULT 0,
   `added_by` INT NULL,
-  `added_at` DATETIME NULL,
+  `added_at` DATETIME NULL DEFAULT NOW(),
   PRIMARY KEY (`project_id`, `user_id`),
   INDEX `fk_project_users_user_id_idx` (`user_id` ASC),
   INDEX `fk_project_users_1_idx` (`added_by` ASC),
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `teama`.`board_users` (
   `user_id` INT NOT NULL,
   `is_admin` TINYINT NULL DEFAULT 0,
   `added_by` INT NULL,
-  `added_at` DATETIME NULL,
+  `added_at` DATETIME NULL DEFAULT NOW(),
   PRIMARY KEY (`board_id`, `user_id`),
   INDEX `fk_board_users_user_id_idx` (`user_id` ASC),
   INDEX `fk_board_users_1_idx` (`added_by` ASC),
@@ -249,8 +249,8 @@ CREATE TABLE IF NOT EXISTS `teama`.`comments` (
   `comment_id` INT NOT NULL AUTO_INCREMENT,
   `item_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `created_at` DATETIME NULL,
-  `comment` TINYTEXT NULL,
+  `created_at` DATETIME NULL DEFAULT NOW(),
+  `comment` TEXT NOT NULL,
   PRIMARY KEY (`comment_id`),
   INDEX `fk_item_id_idx` (`item_id` ASC),
   INDEX `fk_user_id_idx` (`user_id` ASC),
@@ -300,7 +300,7 @@ CREATE TABLE IF NOT EXISTS `teama`.`v_items` (`item_id` INT, `project_id` INT, `
 -- -----------------------------------------------------
 -- Placeholder table for view `teama`.`v_issues`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `teama`.`v_issues` (`item_id` INT, `project_id` INT, `project_name` INT, `board_id` INT, `board_name` INT, `status_id` INT, `status_name` INT, `priority_id` INT, `priority_name` INT, `is_issue` INT, `item_name` INT, `description` INT, `due_date` INT, `time_estimate` INT, `created_by` INT, `created_by_name` INT, `assigned_to` INT, `assigned_to_name` INT, `labels` INT);
+CREATE TABLE IF NOT EXISTS `teama`.`v_issues` (`item_id` INT, `project_id` INT, `project_name` INT, `board_id` INT, `board_name` INT, `status_id` INT, `status_name` INT, `priority_id` INT, `priority_name` INT, `is_issue` INT, `item_name` INT, `description` INT, `due_date` INT, `time_estimate` INT, `created_by` INT, `created_by_name` INT, `assigned_to` INT, `assigned_to_name` INT, `labels` INT, `created_at` INT);
 
 -- -----------------------------------------------------
 -- Placeholder table for view `teama`.`v_comments`
@@ -316,14 +316,16 @@ DROP procedure IF EXISTS `teama`.`add_user`;
 
 DELIMITER $$
 USE `teama`$$
+-- Per NIST best practices accepts passwords up to 64 characters
+-- passwords will be hashed with SHA2 encryption
 CREATE PROCEDURE `add_user` (
   IN in_user_name VARCHAR(45),
-  IN in_password VARBINARY(256),
+  IN in_password VARCHAR(64),
   IN in_email VARCHAR(45),
   OUT out_id INT)
 BEGIN
 INSERT INTO users (user_name, password, email, registered_at)
-  VALUES (in_user_name, in_password, in_email, NOW());
+  VALUES (in_user_name, SHA2(in_password, 256), in_email, NOW());
 SELECT LAST_INSERT_ID() INTO @out_id;
 END$$
 
@@ -391,12 +393,13 @@ DELIMITER $$
 USE `teama`$$
 CREATE PROCEDURE `add_priority` (
   IN in_project_id INT,
+  IN in_priority_rank INT,
   IN in_priority_name VARCHAR(45),
   IN in_description VARCHAR(255),
   OUT out_id INT)
 BEGIN
-INSERT INTO priorities (project_id, priority_name, description)
-  VALUES (in_project_id, in_priority_name, in_description);
+INSERT INTO priorities (project_id, priority_rank, priority_name, description)
+  VALUES (in_project_id, in_priority_rank, in_priority_name, in_description);
 SELECT LAST_INSERT_ID() INTO @out_id;
 END$$
 
@@ -465,12 +468,13 @@ DELIMITER $$
 USE `teama`$$
 CREATE PROCEDURE `add_status` (
   IN in_board_id INT,
+  IN in_status_rank INT,
   IN in_status_name VARCHAR(45),
   IN in_description VARCHAR(255),
   OUT out_id INT)
 BEGIN
-INSERT INTO statuses (board_id, status_name, description)
-  VALUES (in_board_id, in_status_name, in_description);
+INSERT INTO statuses (board_id, status_rank, status_name, description)
+  VALUES (in_board_id, in_status_rank, in_status_name, in_description);
 SELECT LAST_INSERT_ID() INTO @out_id;
 END$$
 
@@ -488,7 +492,7 @@ USE `teama`$$
 CREATE PROCEDURE `add_comment` (
   IN in_item_id INT,
   IN in_user_id INT,
-  IN in_comment TINYTEXT,
+  IN in_comment TEXT,
   OUT out_id INT)
 BEGIN
 INSERT INTO comments (item_id, user_id, created_at, comment)
@@ -614,24 +618,24 @@ DROP procedure IF EXISTS `teama`.`delete_project_user`;
 DELIMITER $$
 USE `teama`$$
 CREATE PROCEDURE `delete_project_user` (
-  IN in_board_id INT,
+  IN in_project_id INT,
   IN in_user_id INT)
 BEGIN
-DELETE FROM board_users WHERE board_id = in_board_id AND user_id = in_user_id;
+DELETE FROM project_users WHERE project_id = in_project_id AND user_id = in_user_id;
 END$$
 
 DELIMITER ;
 
 -- -----------------------------------------------------
--- procedure delete_project_user
+-- procedure delete_board_user
 -- -----------------------------------------------------
 
 USE `teama`;
-DROP procedure IF EXISTS `teama`.`delete_project_user`;
+DROP procedure IF EXISTS `teama`.`delete_board_user`;
 
 DELIMITER $$
 USE `teama`$$
-CREATE PROCEDURE `delete_project_user` (
+CREATE PROCEDURE `delete_board_user` (
   IN in_board_id INT,
   IN in_user_id INT)
 BEGIN
@@ -670,6 +674,337 @@ USE `teama`$$
 CREATE PROCEDURE `delete_comment` (IN in_comment_id INT)
 BEGIN
 DELETE FROM comments WHERE comment_id = in_comment_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_board
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_board`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `edit_board` (
+  IN in_board_id INT,
+  IN in_board_name VARCHAR(45),
+  IN in_description VARCHAR(255))
+BEGIN
+UPDATE boards SET board_name = in_board_name, description = in_description WHERE board_id = in_board_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure grant_board_admin
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`grant_board_admin`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: updates added_by field = in_modified_by, since tracking who granted or revoked
+-- admin privileges is important
+CREATE PROCEDURE `grant_board_admin` (
+  IN in_board_id INT,
+  IN in_user_id INT,
+  IN in_modified_by INT)
+BEGIN
+UPDATE board_users SET is_admin = TRUE, added_by = in_added_by WHERE board_id = in_board_id AND user_id = in_user_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure revoke_board_admin
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`revoke_board_admin`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: updates added_by field = in_modified_by, since tracking who granted or revoked
+-- admin privileges is important
+CREATE PROCEDURE `revoke_board_admin` (
+  IN in_board_id INT,
+  IN in_user_id INT,
+  IN in_modified_by INT)
+BEGIN
+UPDATE board_users SET is_admin = FALSE, added_by = in_modified_by WHERE board_id = in_board_id AND user_id = in_user_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_comment
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_comment`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: you may want to append additional text to the updated comment such
+-- as UPDATED <<timestamp>> so end users can know that the comment has been
+-- updated. We do not do that in the procedure so application can control that
+CREATE PROCEDURE `edit_comment` (
+  IN in_comment_id INT,
+  IN in_comment TEXT)
+BEGIN
+UPDATE comments SET comment = in_comment WHERE comment_id = in_comment_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_item
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_item`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: when editing an item you should probably also add a comment (via add_comment)
+-- showing the old/new values for the modified fields and who modified it for audit
+CREATE PROCEDURE `edit_item` (
+  IN in_item_id INT,
+  IN in_board_id INT,
+  IN in_status_id INT,
+  IN in_priority_id INT,
+  IN in_is_issue TINYINT,
+  IN in_item_name VARCHAR(45),
+  IN in_description MEDIUMTEXT,
+  IN in_due_date DATETIME,
+  IN in_time_estimate INT,
+  IN in_created_by INT,
+  IN in_assigned_to INT,
+  IN in_labels VARCHAR(255))
+BEGIN
+UPDATE items SET board_id = in_board_id, status_id = in_status_id, priority_id = in_priority_id,
+  is_issue = in_is_issue, item_name = in_item_name, description = in_description, due_date = in_due_date,
+  time_estimate = in_time_estimate, created_by = in_created_by, assigned_to = in_assigned_to, labels = in_labels
+WHERE item_id = in_item_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_priority
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_priority`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `edit_priority` (
+  IN in_priority_id INT,
+  IN in_priority_rank INT,
+  IN in_priority_name VARCHAR(45),
+  IN in_description VARCHAR(255))
+BEGIN
+UPDATE priorities SET priority_rank = in_priority_rank, priority_name = in_priority_name, description = in_description
+WHERE priority_id = in_priority_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure delete_priority
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`delete_priority`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `delete_priority` (IN in_priority_id INT)
+BEGIN
+DELETE FROM priorities WHERE priority_id = in_priority_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_status
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_status`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `edit_status` (
+  IN in_status_id INT,
+  IN in_status_rank INT,
+  IN in_status_name VARCHAR(45),
+  IN in_description VARCHAR(255))
+BEGIN
+UPDATE statuses SET status_rank = in_status_rank, status_name = in_status_name, description = in_description
+WHERE status_id = in_status_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure delete_status
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`delete_status`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `delete_status` (IN in_status_id INT)
+BEGIN
+DELETE FROM statuses WHERE status_id = in_status_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure check_user_password_by_user_id
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`check_user_password_by_user_id`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `check_user_password_by_user_id` (
+  IN in_user_id VARCHAR(45),
+  IN in_password VARCHAR(64))
+BEGIN
+-- Looks up user_id and SHA2 encrypted password, returns user_id if there's a match on both
+SELECT user_id FROM users WHERE user_id = in_user_id AND password = SHA2(in_password, 256);
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure check_user_password_by_user_name
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`check_user_password_by_user_name`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `check_user_password_by_user_name` (
+  IN in_user_name VARCHAR(45),
+  IN in_password VARCHAR(64))
+BEGIN
+-- Looks up user_name and SHA2 encrypted password, returns user_id if there's a match on both
+SELECT user_id FROM users WHERE user_name = in_user_name AND password = SHA2(in_password, 256);
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure check_user_password_by_email
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`check_user_password_by_email`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `check_user_password_by_email` (
+  IN in_email VARCHAR(45),
+  IN in_password VARCHAR(64))
+BEGIN
+-- Looks up user_name and SHA2 encrypted password, returns user_id if there's a match on both
+SELECT user_id FROM users WHERE email = in_email AND password = SHA2(in_password, 256);
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_user
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_user`;
+
+DELIMITER $$
+USE `teama`$$
+-- Per NIST best practices accepts passwords up to 64 characters
+-- passwords will be hashed with SHA2 encryption
+CREATE PROCEDURE `edit_user` (
+  IN in_user_id INT,
+  IN in_user_name VARCHAR(45),
+  IN in_password VARCHAR(64),
+  IN in_email VARCHAR(45))
+BEGIN
+UPDATE users SET user_name = in_user_name, password = SHA2(in_password, 256), email = in_email
+WHERE user_id = in_user_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure edit_project
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`edit_project`;
+
+DELIMITER $$
+USE `teama`$$
+CREATE PROCEDURE `edit_project` (
+  IN in_project_id INT,
+  IN in_project_name VARCHAR(45),
+  IN in_description VARCHAR(255))
+BEGIN
+UPDATE projects SET project_name = in_project_name, description = in_description
+WHERE project_id = in_project_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure grant_project_admin
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`grant_project_admin`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: updates added_by field = in_modified_by, since tracking who granted or revoked
+-- admin privileges is important
+CREATE PROCEDURE `grant_project_admin` (
+  IN in_project_id INT,
+  IN in_user_id INT,
+  IN in_modified_by INT)
+BEGIN
+UPDATE project_users SET is_admin = TRUE, added_by = in_modified_by WHERE project_id = in_project_id AND user_id = in_user_id;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure revoke_project_admin
+-- -----------------------------------------------------
+
+USE `teama`;
+DROP procedure IF EXISTS `teama`.`revoke_project_admin`;
+
+DELIMITER $$
+USE `teama`$$
+-- Note: updates added_by field = in_modified_by, since tracking who granted or revoked
+-- admin privileges is important
+CREATE PROCEDURE `revoke_project_admin` (
+  IN in_board_id INT,
+  IN in_user_id INT,
+  IN in_modified_by INT)
+BEGIN
+UPDATE project_users SET is_admin = FALSE, added_by = in_modified_by WHERE project_id = in_project_id AND user_id = in_user_id;
 END$$
 
 DELIMITER ;
